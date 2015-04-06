b0a4be76ef3284a99457a56ce2609279d1c6f1e2
buzzz321 <anders.olme@gmail.com>
2015-04-06 09:55:27 +0200

    added code for body

--- player.js
+++ player.js
@@ -4,21 +4,39 @@ var Player = {
     windowWidth:0,
     windowHeight:0,
     marker: 0,
+    worm: [],
 
     createSprite: function (textureName) {
         // create a texture from an image path
-        var texture = PIXI.Texture.fromImage("marker.png");
+        var texture1 = PIXI.Texture.fromImage("marker.png");
         // create a new Sprite using the texture
-        this.marker = new PIXI.Sprite(texture);
+        this.marker = new PIXI.Sprite(texture1);
+       
+        this.worm.push(this.marker);
         
         // center the sprites anchor point
         this.marker.anchor.x = 0.5;
         this.marker.anchor.y = 0.5;
-        
         // move the sprite t the center of the screen
         this.marker.position.x = 200;
         this.marker.position.y = 150;
-        return this.marker;
+
+        texture = PIXI.Texture.fromImage("ball.png");
+        
+        var ball;
+        for( var n = 0; n < 100; ++n ){
+            ball =  new PIXI.Sprite(texture);
+
+            // center the sprites anchor point
+            ball.anchor.x = 0.5;
+            ball.anchor.y = 0.5;
+        
+            // move the sprite t the center of the screen
+            ball.position.x = 200;
+            ball.position.y = 150;
+            this.worm.push(ball);
+        }
+        return this.worm;
     },
 
     updatePos: function ( ) {
